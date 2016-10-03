0eaf8301a0b914b9a31c2fa74c38580aae5cddd0
Shah, Deven (CCS) <Deven.Shah@capita.co.uk>
2016-10-03 17:08:54 +0100

    Added DataAccess Project and EntityFramework

---
+++ ODataService.DataAccess/ODataServiceContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using ODataService.Models;
+
+namespace ODataService.DataAccess
+{
+    public class ODataServiceContext : DbContext
+    {
+        public DbSet<Country> Countries { get; set; }
+    }
+}
